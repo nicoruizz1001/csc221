d3d97e2ab3d88e13febf78355297b65cc528a802
Nicolas ruiz <nicoruizz1001@gmail.com>
2023-07-16 21:28:25 -0700

    introduce function with parameters, choose own num

--- sheet2/sheet4/numberguess.py
+++ sheet2/sheet4/numberguess.py
@@ -1,23 +1,30 @@
 from random import randint
 
-number = 1000
-again = "yes"
+def guessthenumber(min_val, max_val):
+    number = 100
+    again = "yes"
 
-while again == "yes":
-    number = randint(1, top)
-    print(f"OK, I've thought of a number between 1 and 1000.\n")
-    prompt = "Make a guess: "
-    answer = -1
-    counter = 0
-    while answer != number:
-        answer = int(input(prompt))
-        counter = counter + 1
-        if answer < number:
-            print("That's too low. \n")
-        elif answer > number:
-            print("That's too high dude. \n")
-    print("That was my  number. Well done!")
-    print(f'\nYou took {counter} guesses. ')
+    while again == "yes":
+        number = randint(min_val, max_val)
+        print("OK, I've thought of a number between {min_val} and {max_val}.\n")
+        prompt = "Make a guess: "
+        answer = -1
+        counter = 0
+        while answer != number:
+            answer = int(input(prompt))
+            counter = counter + 1
+            if answer < number:
+                print("That's too low. \n")
+            elif answer > number:
+                print("That's too high. \n")
+        print("That was my number. Well done!")
+        print(f"You took {counter} guesses.\n")
 
-    again = input("Would you to play again? (yes / nope)") .lower().startswith("y")
-print(" bye!")
+        again = input("Would you like to play again? (yes / no) ").lower()
+        if again.startswith("n"):
+            print("Thank you for playing!")
+            break
+
+    print("Goodbye!")
+
+guessthenumber()
